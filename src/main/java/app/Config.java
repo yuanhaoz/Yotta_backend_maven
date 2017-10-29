1da4b0e868e5afdd2f0d2221d34d3e630bb49410
yuanhao <994303>
2017-10-29 15:36:15 +0800

    修改端口为8080

--- src/main/java/app/Config.java
+++ src/main/java/app/Config.java
@@ -16,7 +16,7 @@ public class Config {
      */
     public static String MYSQL_URL = "jdbc:mysql://localhost:3306/yotta_create?user=root&password=root&characterEncoding=UTF8"; // 阿里云服务器：域名+http端口
     public static String IP1 = "http://202.117.54.39"; // 跨域访问控制：域名+apache端口
-    public static String IP2 = "http://202.117.54.39:8081/Yotta"; // 阿里云服务器：域名+http端口
+    public static String IP2 = "http://202.117.54.39:8080/Yotta"; // 阿里云服务器：域名+http端口
 
     public static String DOMAIN_TABLE = "domain";
     public static String FACET_TABLE = "facet";
