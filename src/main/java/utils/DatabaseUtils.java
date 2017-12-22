3a1a046306bc7bbfd13f54786bb076d27d630ddc
郑元浩 <994303805@qq.com>
2017-12-22 10:02:49 +0800

    添加删除数据库某门课程下的所有数据逻辑，修改建表语句链接字段长度为255

--- src/main/java/utils/DatabaseUtils.java
+++ src/main/java/utils/DatabaseUtils.java
@@ -4,6 +4,7 @@ import app.Config;
 import assemble.bean.AssembleFragment2;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -24,6 +25,7 @@ public class DatabaseUtils {
         deleteBadFragment();
         updateSourceName();
         createIndex();
+        deleteDbByClassName();
     }
 
     /**
@@ -65,7 +67,7 @@ public class DatabaseUtils {
         String sqlDomainLayer = "CREATE TABLE IF NOT EXISTS `domain_layer` (\n" +
                 "  `TermID` int(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '术语ID，存储各级未处理的主题（没有含有子主题的主题）',\n" +
                 "  `TermName` varchar(100) DEFAULT NULL COMMENT '术语名',\n" +
-                "  `TermUrl` varchar(100) DEFAULT NULL COMMENT '术语链接',\n" +
+                "  `TermUrl` varchar(255) DEFAULT NULL COMMENT '术语链接',\n" +
                 "  `TermLayer` int(20) DEFAULT NULL COMMENT '术语层级',\n" +
                 "  `ClassName` varchar(100) DEFAULT NULL COMMENT '课程名',\n" +
                 "  PRIMARY KEY (`TermID`)\n" +
@@ -73,7 +75,7 @@ public class DatabaseUtils {
         String sqlDomainLayerFuzhu = "CREATE TABLE IF NOT EXISTS `domain_layer_fuzhu` (\n" +
                 "  `TermID` int(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '术语ID，存储各级未处理的主题（存储含有子主题的主题）',\n" +
                 "  `TermName` varchar(100) DEFAULT NULL COMMENT '术语名',\n" +
-                "  `TermUrl` varchar(100) DEFAULT NULL COMMENT '术语链接',\n" +
+                "  `TermUrl` varchar(255) DEFAULT NULL COMMENT '术语链接',\n" +
                 "  `TermLayer` int(20) DEFAULT NULL COMMENT '术语层级',\n" +
                 "  `isCatalog` int(20) DEFAULT NULL COMMENT '该主题是不是Catalog页面的主题：1表示是，0表示不是',\n" +
                 "  `ClassName` varchar(100) DEFAULT NULL COMMENT '课程名',\n" +
@@ -82,7 +84,7 @@ public class DatabaseUtils {
         String sqlDomainLayerFuzhu2 = "CREATE TABLE IF NOT EXISTS `domain_layer_fuzhu2` (\n" +
                 "  `TermID` int(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '术语ID，删除重复的主题（含有子主题的主题）',\n" +
                 "  `TermName` varchar(100) DEFAULT NULL COMMENT '术语名',\n" +
-                "  `TermUrl` varchar(100) DEFAULT NULL COMMENT '术语链接',\n" +
+                "  `TermUrl` varchar(255) DEFAULT NULL COMMENT '术语链接',\n" +
                 "  `TermLayer` int(20) DEFAULT NULL COMMENT '术语层级',\n" +
                 "  `isCatalog` int(20) DEFAULT NULL COMMENT '该主题是不是Catalog页面的主题：1表示是，0表示不是',\n" +
                 "  `ClassName` varchar(100) DEFAULT NULL COMMENT '课程名',\n" +
@@ -100,7 +102,7 @@ public class DatabaseUtils {
         String sqlDomainTopic = "CREATE TABLE IF NOT EXISTS `domain_topic` (\n" +
                 "  `TermID` int(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主题ID',\n" +
                 "  `TermName` varchar(100) DEFAULT NULL COMMENT '主题名',\n" +
-                "  `TermUrl` varchar(100) DEFAULT NULL COMMENT '主题链接',\n" +
+                "  `TermUrl` varchar(255) DEFAULT NULL COMMENT '主题链接',\n" +
                 "  `TermLayer` int(20) DEFAULT NULL COMMENT '主题层级',\n" +
                 "  `ClassName` varchar(100) DEFAULT NULL COMMENT '课程名',\n" +
                 "  `ClassID` int(20) DEFAULT NULL COMMENT '课程ID',\n" +
@@ -154,7 +156,7 @@ public class DatabaseUtils {
         String sqlUnaddImage = "CREATE TABLE IF NOT EXISTS `unadd_image` (\n" +
                 "  `ImageID` int(20) NOT NULL AUTO_INCREMENT COMMENT '新增图片ID',\n" +
                 "  `ImageContent` longblob COMMENT '新增图片内容',\n" +
-                "  `ImageUrl` varchar(100) DEFAULT NULL COMMENT '新增图片链接',\n" +
+                "  `ImageUrl` varchar(255) DEFAULT NULL COMMENT '新增图片链接',\n" +
                 "  `ImageScratchTime` datetime DEFAULT NULL COMMENT '新增图片上传时间',\n" +
                 "  `ImageAPI` varchar(100) DEFAULT NULL COMMENT '新增图片API',\n" +
                 "  PRIMARY KEY (`ImageID`)\n" +
@@ -308,6 +310,100 @@ public class DatabaseUtils {
             e.printStackTrace();
         }
 
+        mysql.closeconnection();
+    }
+
+    public static void deleteDbByClassName() {
+        String domainName = "理论物理学家";
+        // 删除数据库中这门课程的数据
+        List<String> tableList = new ArrayList<>();
+        tableList.add(Config.DOMAIN_TABLE);
+        tableList.add(Config.DOMAIN_LAYER_TABLE);
+        tableList.add(Config.DOMAIN_LAYER_FUZHU_TABLE);
+        tableList.add(Config.DOMAIN_LAYER_FUZHU2_TABLE);
+        tableList.add(Config.DOMAIN_LAYER_RELATION_TABLE);
+        tableList.add(Config.DOMAIN_TOPIC_TABLE);
+        tableList.add(Config.DOMAIN_TOPIC_RELATION_TABLE);
+        tableList.add(Config.FACET_TABLE);
+        tableList.add(Config.FACET_RELATION_TABLE);
+        tableList.add(Config.ASSEMBLE_FRAGMENT_TABLE);
+        tableList.add(Config.DEPENDENCY);
+        for (int i = 0; i < tableList.size(); i++) {
+            deleteByTableAndDomain(tableList.get(i), domainName);
+        }
+        // 更新数据库表格自动增长ID的值
+        HashMap<String, Integer> map = getMaxId();
+        resetTableIncrement(map);
+    }
+
+    /**
+     * 根据领域名删除数据
+     * @param table 数据库表格名
+     * @param domainName 领域名
+     */
+    public static void deleteByTableAndDomain(String table, String domainName) {
+        mysqlUtils mysql = new mysqlUtils();
+        String sql = "delete from " + table + " where ClassName = ?";
+        List<Object> params = new ArrayList<Object>();
+        params.add(domainName);
+        try {
+            mysql.addDeleteModify(sql, params);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        mysql.closeconnection();
+    }
+
+    /**
+     * 获取所有表格当前最大的编号值+1，设置每个表格自动增长的值为该值
+     */
+    public static HashMap<String, Integer> getMaxId() {
+        HashMap<String, Integer> map = new HashMap<>();
+        mysqlUtils mysql = new mysqlUtils();
+        List<Object> params = new ArrayList<>();
+        String sqlDomain = "select max(ClassID) as maxClassID from " + Config.DOMAIN_TABLE;
+        String sqlDomainLayer = "select max(TermID) as maxTermID from " + Config.DOMAIN_LAYER_TABLE;
+        String sqlDomainLayerFuzhu = "select max(TermID) as maxTermID from " + Config.DOMAIN_LAYER_FUZHU_TABLE;
+        String sqlDomainLayerFuzhu2 = "select max(TermID) as maxTermID from " + Config.DOMAIN_LAYER_FUZHU2_TABLE;
+        String sqlDomainLayerRelation = "select max(TopicRelationId) as maxTopicRelationId from " + Config.DOMAIN_LAYER_RELATION_TABLE;
+        String sqlDomainTopic = "select max(TermID) as maxTermID from " + Config.DOMAIN_TOPIC_TABLE;
+        String sqlAssembleFragment = "select max(FragmentID) as maxFragmentID from " + Config.ASSEMBLE_FRAGMENT_TABLE;
+        try {
+            map.put(Config.DOMAIN_TABLE, Integer.parseInt(mysql.returnMultipleResult(sqlDomain, params).get(0).get("maxClassID").toString()) + 1);
+            map.put(Config.DOMAIN_LAYER_TABLE, Integer.parseInt(mysql.returnMultipleResult(sqlDomainLayer, params).get(0).get("maxTermID").toString()) + 1);
+            map.put(Config.DOMAIN_LAYER_FUZHU_TABLE, Integer.parseInt(mysql.returnMultipleResult(sqlDomainLayerFuzhu, params).get(0).get("maxTermID").toString()) + 1);
+            map.put(Config.DOMAIN_LAYER_FUZHU2_TABLE, Integer.parseInt(mysql.returnMultipleResult(sqlDomainLayerFuzhu2, params).get(0).get("maxTermID").toString()) + 1);
+            map.put(Config.DOMAIN_LAYER_RELATION_TABLE, Integer.parseInt(mysql.returnMultipleResult(sqlDomainLayerRelation, params).get(0).get("maxTopicRelationId").toString()) + 1);
+            map.put(Config.DOMAIN_TOPIC_TABLE, Integer.parseInt(mysql.returnMultipleResult(sqlDomainTopic, params).get(0).get("maxTermID").toString()) + 1);
+            map.put(Config.ASSEMBLE_FRAGMENT_TABLE, Integer.parseInt(mysql.returnMultipleResult(sqlAssembleFragment, params).get(0).get("maxFragmentID").toString()) + 1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        mysql.closeconnection();
+
+        for (String table : map.keySet()) {
+            System.out.println(table + " \t --> 最大编号为：" + map.get(table));
+        }
+
+        return map;
+    }
+
+    /**
+     * 更新每个表格的auto_increment值
+     * @param map
+     */
+    public static void resetTableIncrement(HashMap<String, Integer> map) {
+        mysqlUtils mysql = new mysqlUtils();
+        for (String table : map.keySet()) {
+            String sql = "alter table " + table + " auto_increment = ?";
+            List<Object> params = new ArrayList<>();
+            params.add(map.get(table));
+            try {
+                mysql.addDeleteModify(sql, params);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
         mysql.closeconnection();
     }
 
