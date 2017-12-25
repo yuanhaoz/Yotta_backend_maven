907b30b53a21428023881605942d6e767f451c2b
yuanhao <994303>
2017-12-25 21:08:34 +0800

    branch-001：1.SpiderAPI.java修改上传文件api和爬取多门课程api；2.SpiderAPI修改人工添加碎片api，需要添加Text和SourceName字段，并且设置数据库Text

--- README.md
+++ README.md
@@ -3,3 +3,28 @@
 Yotta后端
 
 采用maven重新整理，并推荐使用idea进行开发
+
+## 环境配置
+
+1. 配置修改
+    1. app目录下Config.java文件为配置文件，需要修改。
+    2. IDEA运行时配置tomcat服务器，在Deployment中添加"--:war exploded"并设置Application context为工程目录："/Yotta"；同时配置"--:war"，路径为"/"即可。启动后在target下生成对应的Yotta.war文件。
+    3. server运行的端口和最终需要放到的tomcat服务端口保持一致，这样生成的war包可以直接放到tomcat服务器的webapps下即可正常工作。
+    4. 启动tomcat时，如果报错：Caused by: java.lang.NoClassDefFoundError: javax/ws/rs/ProcessingException。由于jersey包没有导入完全，“project setting”->“Artifacts”->“* exploded”->“WEB-INF”->“lib”中没有把“javax.ws.rs-api:2.1”这个包加到tomcat的运行环境中，导入启动tomcat的过程出错，因此需要手动点击添加，随后即可正常启动tomcat服务器。[参考链接](http://blog.csdn.net/u012428012/article/details/78831290)
+    
+2. 手动添加ZHConverter.jar到本地maven仓库，命令为：mvn install:install-file -Dfile=ZHConverter-2.6.12.jar -DgroupId=com.spreada.utils -DartifactId=ZHConverter -Dversion=2.6.12 -Dpackaging=jar
+
+3. 数据库设置
+    1. 配置文件my.ini设置数据传输大小限制：max_allowed_packet=512M，重启mysql服务。
+    2. 设置数据库表格为MyISAM引擎。
+    3. 给表格建立索引，加快访问速度；varchar索引字段长度为100。
+    
+4. 爬虫设置
+    1. 设置IEDriverServer.exe到Config.IE_PATH路径下，默认是放在D盘下。
+    2. 设置IE浏览器的缩放为100%。
+    3. “Internet选项”->“安全”：四个安全设置都启用保护模式。
+    4. 单个爬虫：输入学科和课程即可。如：“计算机科学”，“数据结构”。
+    5. 批量爬虫上传文件要求：
+        1. xls格式的excel文件。
+        2. 表格包含两列，第一行分别为：“学科”、“课程名”。
+        3. 第二行开始为具体的学科和课程信息，如：“计算机科学”，“数据结构”。
--- src/main/java/spider/SpiderAPI.java
+++ src/main/java/spider/SpiderAPI.java
@@ -5,18 +5,23 @@ import app.error;
 import app.success;
 import domain.bean.Domain;
 import io.swagger.annotations.*;
+import org.apache.commons.io.FileUtils;
+import org.eclipse.jetty.util.MultiPartInputStream;
 import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 import org.glassfish.jersey.media.multipart.FormDataParam;
 import spider.bean.AssembleFragment;
 import spider.spiders.SpidersRun;
 import spider.spiders.wikicn.MysqlReadWriteDAO;
 import utils.DatabaseUtils;
+import utils.JsoupDao;
 import utils.Log;
 import utils.mysqlUtils;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.*;
@@ -30,6 +35,44 @@ import java.util.*;
 public class SpiderAPI {
 
     @POST
+    @Path("/uploadFile")
+    @ApiOperation(value = "上传爬虫excel", notes = "上传爬虫excel")
+    @ApiResponses(value = {
+            @ApiResponse(code = 401, message = "MySql数据库  查询失败"),
+            @ApiResponse(code = 200, message = "MySql数据库  查询成功", response = String.class)})
+    @Consumes(MediaType.MULTIPART_FORM_DATA + ";charset=" + "UTF-8")
+    @Produces(MediaType.APPLICATION_JSON + ";charset=" + "UTF-8")
+    public static Response uploadFile(
+            @FormDataParam("file") FormDataContentDisposition disposition,
+            @FormDataParam("file") InputStream fileInputStream
+    ) throws Exception {
+        Map<String, String> map = new HashMap<>();
+        Response response = null;
+//        Log.log(disposition.getFileName() + ", " + disposition.getType() + ", "  + disposition.getSize());
+        // 判断文件是否为空
+        if (fileInputStream != null) {
+            try {
+                // 文件保存路径
+                File path = new File(SpiderAPI.class.getClassLoader().getResource("").getPath() + "/upload");
+                if (!path.exists()) {
+                    boolean status = path.mkdirs();
+                }
+                String filePath = path + "/" + disposition.getFileName();
+                Log.log("爬虫excel保存路径为：" + filePath);
+                // 转存文件
+                FileUtils.copyInputStreamToFile(fileInputStream, new File(filePath));
+                map.put("msg", "上传成功");
+                response = Response.status(200).entity(map).build();
+            } catch (Exception e) {
+                Log.log(e.getMessage());
+                map.put("msg", "上传失败");
+                response = Response.status(401).entity(map).build();
+            }
+        }
+        return response;
+    }
+
+    @GET
     @Path("/startSpiders")
     @ApiOperation(value = "启动爬虫", notes = "输入课程信息excel文件，启动爬虫")
     @ApiResponses(value = {
@@ -38,14 +81,15 @@ public class SpiderAPI {
     @Consumes("application/x-www-form-urlencoded" + ";charset=" + "UTF-8")
     @Produces(MediaType.APPLICATION_JSON + ";charset=" + "UTF-8")
     public static Response startSpiders(
-            @FormDataParam("imageContent") FormDataContentDisposition disposition,
-            @FormDataParam("imageContent") InputStream fileInputStream
+            @ApiParam(value = "文件名", required = true) @QueryParam("fileName") String fileName
     ) throws Exception {
         Response response = null;
         // 如果数据库中表格不存在，先新建数据库表格
         DatabaseUtils.createTable();
         // 爬取多门课程
-        List<Domain> domainList = SpidersRun.getDomainFromInputStream(fileInputStream);
+        String filePath = SpiderAPI.class.getClassLoader().getResource("").getPath() + "\\upload\\" + fileName;
+        Log.log("加载爬虫所需的课程excel文件：" + filePath);
+        List<Domain> domainList = SpidersRun.getDomainFromExcel(filePath);
         for (int i = 0; i < domainList.size(); i++) {
             Log.log(domainList.get(i));
             SpidersRun.constructKGByDomainName(domainList.get(i));
@@ -740,7 +784,7 @@ public class SpiderAPI {
             String sql_term = "select * from " + Config.DOMAIN_TOPIC_TABLE + " where ClassName=? and TermName=?";
             String sql_query = "select * from " + Config.FRAGMENT + " where FragmentID=?";
             String sql_delete = "delete from " + Config.FRAGMENT + " where FragmentID=?";
-            String sql_add = "insert into " + Config.ASSEMBLE_FRAGMENT_TABLE + "(FragmentContent,FragmentScratchTime,TermID,TermName,FacetName,FacetLayer,ClassName) values(?,?,?,?,?,?,?);";
+            String sql_add = "insert into " + Config.ASSEMBLE_FRAGMENT_TABLE + "(FragmentContent,Text,FragmentScratchTime,TermID,TermName,FacetName,FacetLayer,ClassName,SourceName) values(?,?,?,?,?,?,?,?,?);";
 
             List<Object> params_term = new ArrayList<Object>();
             params_term.add(ClassName);
@@ -752,12 +796,14 @@ public class SpiderAPI {
                 List<Map<String, Object>> fragmentinfo = mysql.returnMultipleResult(sql_query, params_fragment);
                 List<Object> params_add = new ArrayList<Object>();
                 params_add.add(fragmentinfo.get(0).get("FragmentContent"));
+                params_add.add(JsoupDao.parseHtmlText(fragmentinfo.get(0).get("FragmentContent").toString()).text());
                 params_add.add(fragmentinfo.get(0).get("FragmentScratchTime"));
                 params_add.add(results_term.get(0).get("TermID"));
                 params_add.add(TermName);
                 params_add.add(FacetName);
                 params_add.add(FacetLayer);
                 params_add.add(ClassName);
+                params_add.add("人工");
                 result = mysql.addDeleteModify(sql_add, params_add);
                 if (result) {
                     try {
